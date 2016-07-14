1fe5f438493cdccbd470db3a61e1cb20280ff01c
Kostegan <churilovkonstantin@gmail.com>
2016-07-15 00:00:32 +0400

    Added class comment

--- src/main/java/com/light/Main.java
+++ src/main/java/com/light/Main.java
@@ -1,18 +1,14 @@
 package com.light;
 
-import com.light.trafficlight.ILight;
 import com.light.trafficlight.ITrafficLight;
 import com.light.trafficlight.TrafficLight;
 
 /**
- *
+ * Simply displays color by time.
  */
 public class Main {
     public static void main(String[] args) {
-//        ITrafficLight trafficLight = new TrafficLight();
-//        trafficLight.printColorByMinute();
-
-        ILight trafficLight = new TrafficLight();
-        trafficLight.printColorByMinuteTwo();
+        ITrafficLight trafficLight = new TrafficLight();
+        trafficLight.printColorByMinute();
     }
 }
