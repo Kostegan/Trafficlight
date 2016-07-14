fef283d51a7a8b6badc5b9b15e97040668503a4d
Kostegan <churilovkonstantin@gmail.com>
2016-07-14 23:59:27 +0400

    Added class MainTwo with a new realization getColorByTime

---
+++ src/main/java/com/light/MainTwo.java
@@ -0,0 +1,15 @@
+package com.light;
+
+import com.light.trafficlight.ILight;
+import com.light.trafficlight.TrafficLight;
+
+/**
+ * Offers set a glow time for every colors.
+ */
+public class MainTwo {
+    public static void main(String[] args) {
+        ILight trafficLight = new TrafficLight();
+        trafficLight.printColorByMinuteTwo();
+    }
+
+}
