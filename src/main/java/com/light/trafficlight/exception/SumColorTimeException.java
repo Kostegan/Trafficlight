d3ef9068ff30bb49786368dac4eb9ff9d99fc413
Kostegan <churilovkonstantin@gmail.com>
2016-07-15 00:52:11 +0400

    Added new exception, if sum glow-times less a specified time.

---
+++ src/main/java/com/light/trafficlight/exception/SumColorTimeException.java
@@ -0,0 +1,10 @@
+package com.light.trafficlight.exception;
+
+/**
+ *
+ */
+public class SumColorTimeException extends Exception {
+    public SumColorTimeException(String message){
+        super(message);
+    }
+}
